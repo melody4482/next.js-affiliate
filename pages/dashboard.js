a09a137b0871ba3fe16e2b1307abb9676ff5791b
WebMelody <dev.mattew.chen@gmail.com>
2023-02-22 13:50:11 -0800

    removed parsefloat

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -54,7 +54,7 @@ const columns = [
         id: 'revenue',
         align: 'center',
         label: 'Revenue',
-        render: revenue => <p>{isEmpty(revenue) ? 0 : `$${parseFloat(revenue).toFixed(2)}`}</p>
+        render: revenue => <p>{isEmpty(revenue) ? 0 : `$${revenue.toFixed(2)}`}</p>
     },
     // {
     //     id: 'offer',
@@ -65,7 +65,7 @@ const columns = [
         id: 'spend',
         align: 'center',
         label: 'Spend',
-        render: spend => <p>{isEmpty(spend) ? 0 : `$${parseFloat(spend).toFixed(2)}`}</p>
+        render: spend => <p>{isEmpty(spend) ? 0 : `$${spend.toFixed(2)}`}</p>
     },
     {
         id: 'profit',
@@ -76,16 +76,16 @@ const columns = [
         },
         render: profit => 
                 <p 
-                    style={parseFloat(profit) < 0 ? {color: 'red'} : {color: '#fff'}}
+                    style={profit < 0 ? {color: 'red'} : {color: '#fff'}}
                 >
-                    {isEmpty(profit) ? 0 : `$${parseFloat(profit).toFixed(2)}`}
+                    {isEmpty(profit) ? 0 : `$${profit.toFixed(2)}`}
                 </p>
     },
     {
         id: 'roas',
         align: 'center',
         label: 'ROAS',
-        render: roas => <p>{isEmpty(roas) ? `0%` : `$${parseInt(roas * 100)}%`}</p>
+        render: roas => <p>{isEmpty(roas) ? `0%` : `$${roas * 100}%`}</p>
     },
 ];
 
