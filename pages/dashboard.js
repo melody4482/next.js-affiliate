91dbe6f636c36facbdb13a02268d5c1b4ac13148
WebMelody <dev.mattew.chen@gmail.com>
2023-02-22 13:09:31 -0800

    added accounts

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -85,7 +85,9 @@ const tiktokAccounts = [
     { name: "BGM - Media Digital", value: '7145102062467006466' },
     { name: "Plug Co", value: '7060636350755667969' },
     { name: "Stacks Ads", value: '7060955335363805185' },
-    { name: "Plug 2022", value: '7063596340252573698' }
+    { name: "Plug 2022", value: '7063596340252573698' },
+    { name: "Pluggy", value: '7054703945205170178' },
+    { name: "New Plug", value: '7068326186375528449' }
 ];
 
 const plugAccounts = [
